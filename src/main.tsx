77d7f480274e93069db469d98142b694fa635838
hanshs <hansh.starkopf@gmail.com>
2023-03-31 15:02:04 +0300

    remove mathlive assets import

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-import 'mathlive/dist/mathlive-fonts.css';
-import 'mathlive/dist/mathlive-static.css';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
